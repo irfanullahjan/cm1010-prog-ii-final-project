2d0dfbf0ab5b8f9058420e949d2d8543cfa3927f
irfanullahjan <irfanullah.jan@outlook.com>
2020-01-05 15:37:56 +0500

    Initial

---
+++ road-vehicles-miles.js
@@ -0,0 +1,197 @@
+function RoadVehiclesMiles() {
+
+    // Name for the visualisation to appear in the menu bar.
+    this.name = 'Road Vehicles: 1949 to 2018';
+
+    // Each visualisation must have a unique ID with no special
+    // characters.
+    this.id = 'road-vehicles-miles';
+
+    // Title to display above the plot.
+    this.title = 'Road vehicles: Billion miles travelled in a year';
+
+    // Names for each axis.
+    this.xAxisLabel = 'Year';
+    this.yAxisLabel = 'Billion Miles';
+
+    var marginSize = 35;
+
+    // Layout object to store all common plot layout parameters and
+    // methods.
+    this.layout = {
+        marginSize: marginSize,
+
+        // Locations of margin positions. Left and bottom have double margin
+        // size due to axis and tick labels.
+        leftMargin: marginSize * 2,
+        rightMargin: width - marginSize,
+        topMargin: marginSize,
+        bottomMargin: height - marginSize * 2,
+        pad: 5,
+
+        plotWidth: function () {
+            return this.rightMargin - this.leftMargin;
+        },
+
+        plotHeight: function () {
+            return this.bottomMargin - this.topMargin;
+        },
+
+        // Boolean to enable/disable background grid.
+        grid: false,
+
+        // Number of axis tick labels to draw so that they are not drawn on
+        // top of one another.
+        numXTickLabels: 10,
+        numYTickLabels: 8,
+    };
+
+    // Property to represent whether data has been loaded.
+    this.loaded = false;
+
+    // Preload the data. This function is called automatically by the
+    // gallery when a visualisation is added.
+    this.preload = function () {
+        var self = this;
+        this.data = loadTable(
+            './data/road-vehicles/road-vehicle-miles.csv', 'csv', 'header',
+            // Callback function to set the value
+            // this.loaded to true.
+            function (inputdata) {
+                self.loaded = true;
+                // Loops to accumulate numbers in columns. Cosecutive columns are added together.
+                for (var i = 0; i < inputdata.getRowCount(); i++) {
+                    for (var j=0; j < inputdata.getColumnCount(); j++)
+                    if (j>1) {
+                        inputdata.setNum(i, j, inputdata.getNum(i,j-1)+inputdata.getNum(i,j));
+                    }
+                }
+                return inputdata;
+            });
+    };
+
+    this.setup = function () {
+        // Font defaults.
+        textSize(16);
+        //Cumulatively adds columns to create stacked chart
+
+        // Set min and max years: assumes data is sorted by date.
+        this.startYear = this.data.getNum(0, 'Year');
+        this.endYear = this.data.getNum(this.data.getRowCount() - 1, 'Year');
+
+        // Find min and max pay gap for mapping to canvas height.
+        this.minTraffic = 0;         // Pay equality (zero pay gap).
+        this.maxTraffic = max(this.data.getColumn(this.data.getColumnCount()-1));
+    };
+
+    this.destroy = function () {
+    };
+
+    this.draw = function () {
+        if (!this.loaded) {
+            console.log('Data not yet loaded');
+            return;
+        }
+
+        // Draw the title above the plot.
+        this.drawTitle();
+
+        // Draw all y-axis labels.
+        drawYAxisTickLabels(this.minTraffic,
+            this.maxTraffic,
+            this.layout,
+            this.maprafficToHeight.bind(this),
+            0);
+
+        // Draw x and y axis.
+        drawAxis(this.layout);
+
+        // Draw x and y axis labels.
+        drawAxisLabels(this.xAxisLabel,
+            this.yAxisLabel,
+            this.layout);
+
+        // Plot all pay gaps between startYear and endYear using the width
+        // of the canvas minus margins.
+        var previous;
+        var numYears = this.endYear - this.startYear;
+
+        var fillclr = ['purple', 'blue', 'green', 'yellow','orange','red']
+
+        // Loop over all rows and draw a line from the previous value to
+        // the current.
+        for (var j = this.data.getColumnCount() - 1; j > 0; j--) {
+            previous = null;
+            beginShape();
+            for (var i = 0; i < this.data.getRowCount(); i++) {
+                fill(fillclr[j]);
+                noStroke();
+                // Create an object to store data for the current year.
+                var current = {
+                    // Convert strings to numbers.
+                    'year': this.data.getNum(i, 0),
+                    'Traffic': this.data.getNum(i, j)
+                };
+                vertex(
+                    this.mapYearToWidth(current.year),
+                    this.maprafficToHeight(current.Traffic)
+                );
+                if (previous != null) {
+                    // Draw line segment connecting previous year to current
+                    // year pay gap.
+
+
+                    // The number of x-axis labels to skip so that only
+                    // numXTickLabels are drawn.
+                    var xLabelSkip = ceil(numYears / this.layout.numXTickLabels);
+
+                    // Draw the tick label marking the start of the previous year.
+                    if (i % xLabelSkip == 0) {
+                        drawXAxisTickLabel(previous.year, this.layout,
+                            this.mapYearToWidth.bind(this));
+                    }
+                }
+
+                // Assign current year to previous year so that it is available
+                // during the next iteration of this loop to give us the start
+                // position of the next line segment.
+                previous = current;
+            }
+            vertex(
+                this.mapYearToWidth(this.endYear),
+                this.maprafficToHeight(0)
+            );
+            vertex(
+                this.mapYearToWidth(this.startYear),
+                this.maprafficToHeight(0)
+            );
+            endShape();
+        }
+    };
+
+    this.drawTitle = function () {
+        fill(0);
+        noStroke();
+        textAlign('center', 'center');
+
+        text(this.title,
+            (this.layout.plotWidth() / 2) + this.layout.leftMargin,
+            this.layout.topMargin - (this.layout.marginSize / 2));
+    };
+
+    this.mapYearToWidth = function (value) {
+        return map(value,
+            this.startYear,
+            this.endYear,
+            this.layout.leftMargin,   // Draw left-to-right from margin.
+            this.layout.rightMargin);
+    };
+
+    this.maprafficToHeight = function (value) {
+        return map(value,
+            this.minTraffic,
+            this.maxTraffic,
+            this.layout.bottomMargin, // Smaller pay gap at bottom.
+            this.layout.topMargin);   // Bigger pay gap at top.
+    };
+}
---
+++ tech-diversity-race.js
@@ -0,0 +1,90 @@
+function TechDiversityRace() {
+
+    // Name for the visualisation to appear in the menu bar.
+    this.name = 'Tech Diversity: Race';
+
+    // Each visualisation must have a unique ID with no special
+    // characters.
+    this.id = 'tech-diversity-race';
+
+    // Property to represent whether data has been loaded.
+    this.loaded = false;
+
+    // Values in this array are incremented smoothly to created animation effect. Initial values are given so the chart animates on load
+    this.currentState = [60,60,60,60,60,60];
+    // Values by which to increment currentState each frame in order to reach targetState
+    this.incrementArr = [0,0,0,0,0,0];
+
+    // Preload the data. This function is called automatically by the
+    // gallery when a visualisation is added.
+    this.preload = function () {
+        var self = this;
+        this.data = loadTable(
+            './data/tech-diversity/race-2018.csv', 'csv', 'header',
+            // Callback function to set the value
+            // this.loaded to true.
+            function (table) {
+                self.loaded = true;
+            });
+    };
+
+    this.setup = function () {
+        if (!this.loaded) {
+            console.log('Data not yet loaded');
+            return;
+        }
+
+        // Create a select DOM element.
+        this.select = createSelect();
+        this.select.position(350, 40);
+
+        // Fill the options with all company names.
+        var companies = this.data.columns;
+        // First entry is empty.
+        for (let i = 1; i < companies.length; i++) {
+            this.select.option(companies[i]);
+        }
+    };
+
+    this.destroy = function () {
+        this.select.remove();
+        this.currentState = [60,60,60,60,60,60]; //resets pie to equal sections so it animates when visualization is clicked again
+    };
+
+    // Create a new pie chart object.
+    this.pie = new PieChart(width / 2, height / 2, width * 0.4);
+
+    this.draw = function () {
+        if (!this.loaded) {
+            console.log('Data not yet loaded');
+            return;
+        }
+
+        // Get the value of the company we're interested in from the
+        // select item.
+        var companyName = this.select.value();
+
+        // Get the column of raw data for companyName.
+        var targetState = this.data.getColumn(companyName);
+
+        // Convert all data strings to numbers.
+        targetState = stringsToNumbers(targetState);
+
+        for (var i = 0; i < this.currentState.length; i++) {
+            this.incrementArr[i] = (targetState[i] - this.currentState[i]) / 15;       // averages the animation over the next 15 frames. actually takes much longer than 15 frames to reach target
+            this.currentState[i] = this.currentState[i] + this.incrementArr[i];        // Increment values in currentState by values in incrementsArr
+        }
+
+        // Copy the row labels from the table (the first item of each row).
+        var labels = this.data.getColumn(0);
+
+        // Colour to use for each category.
+        var colours = ['blue', 'red', 'green', 'pink', 'purple', 'yellow'];
+
+        // Make a title.
+        var title = 'Employee diversity at ' + companyName;
+
+        // Draw the pie chart!
+        this.pie.draw(this.currentState, labels, colours, title);
+    };
+}
